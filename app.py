c159169aca74ea587cdbc57af54a66d2b7cca9cc
AllanCFE <allan.echeverria@gmail.com>
2023-07-21 00:46:36 -0300

    feat: Done history and Undo function

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -14,10 +15,21 @@ class Todo(db.Model):
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
+class Done(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    completeDate = db.Column(db.DateTime)
+
 @app.route("/")
 def home():
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
+
+@app.route("/done")
+def done():
+    done_list = Done.query.all()
+    return render_template("done.html", done_list=done_list)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -31,6 +43,26 @@ def add():
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
+
+    done = Done(title=todo.title, complete=True)
+    done.completeDate = datetime.datetime.now()
+    db.session.add(done)
+
+    db.session.delete(todo)
+
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/undo/<int:todo_id>")
+def undo(todo_id):
+    done = Done.query.filter_by(id=todo_id).first()
+    done.complete = not done.complete
+
+    todo = Todo(title=done.title, complete=False)
+    db.session.add(todo)
+
+    db.session.delete(done)
+
     db.session.commit()
     return redirect(url_for("home"))
 
